654b7744c8ddcb2e7f5cd61185056d0114a59d28
Michael J Feher <github@phearzero.com>
2023-04-22 18:38:04 -0500

    🎨 Add Dashboard and default index

---
+++ src/lib/Dashboard.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Nodes from "$lib/bmc/Nodes.svelte";
+    import Server from "$lib/bmc/Server.svelte";
+    import USB from "$lib/bmc/USB.svelte";
+
+</script>
+
+<article>
+    <header>
+        <div class="row">
+            <hgroup class="col-8">
+                <h1>Dashboard</h1>
+                <h2>Settings from the Baseboard Management Controller</h2>
+            </hgroup>
+        </div>
+    </header>
+    <Nodes/>
+    <USB/>
+    <Server/>
+
+</article>
+
+<style>
+    hgroup,
+    article {
+        margin: 0;
+    }
+</style>
