9c05d0341580fbc0bdfb0e3b8633883a3cb4bab8
Michael J Feher <github@phearzero.com>
2023-04-28 11:23:33 -0500

    💄 Add loading/error screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,43 @@
 <script>
-    import './styles.css'
-    import './pico-bootstrap-grid.css'
-    import Navigation from "../lib/Navigation.svelte";
-    import Footer from "../lib/Footer.svelte";
+	import './styles.css';
+	import './pico-bootstrap-grid.css';
+	import Navigation from '$lib/Navigation.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import {server} from '$lib/stores/server'
 </script>
+{#await server.init()}
+	<div aria-busy='true' class='container-fluid loading'></div>
+{:then s}
+	<Navigation />
+	<main class="container-fluid">
+		<slot />
+	</main>
+	<Footer />
+{:catch error}
+<div class='container-fluid error-message'>
+	<hgroup>
+		<h1>Error</h1>
+		<h2>{error.message}</h2>
+	</hgroup>
+</div>
+{/await}
 
-<Navigation/>
-<main class="container-fluid">
-    <slot/>
-</main>
-<Footer/>
+
 <style>
-    main {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        min-height: calc(100vh - 7rem);
-        padding: 0;
-    }
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		min-height: calc(100vh - 7rem);
+		padding: 0;
+	}
+	.loading, .error-message {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			height: 100%;
+	}
+	.loading {
+      font-size: 10rem;
+	}
 </style>
