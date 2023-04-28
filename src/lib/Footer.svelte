2fcd765e5267f58d428677a20da93b54f2e4fd72
Michael Feher <git@phearzero.com>
2023-04-27 21:51:46 -0500

    💄 Flatten app for Pico

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,3 +3,9 @@
         Built with <a href="https://kit.svelte.dev/" class="secondary">SvelteKit</a> and <a href="https://picocss.com" class="secondary">Pico</a>
     </small>
 </footer>
+
+<style>
+    footer {
+        padding: 15px;
+    }
+</style>
